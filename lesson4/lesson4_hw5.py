51f85645a3dfe76115b81d15fce2c0166f763e47
sitdikov_denis <fomk@mail.ru>
2020-10-15 22:37:43 +0300

    [upd] сделал пятое задание четвертого урока

--- lesson4/lesson4_hw5.py
+++ lesson4/lesson4_hw5.py
@@ -4,3 +4,15 @@
 Необходимо получить результат вычисления произведения всех элементов списка.
 Подсказка: использовать функцию reduce().
 """
+from functools import reduce
+
+my_list = [elem for elem in range(100, 1001) if elem % 2 == 0]
+
+print(my_list)
+
+
+def my_mult(prev_el, el):
+    return prev_el * el
+
+
+print(reduce(my_mult, my_list))
