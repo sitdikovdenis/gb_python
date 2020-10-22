ddd58c036ccdc223d80ba2ba3bdec1f498d9f8ad
sitdikov_denis <fomk@mail.ru>
2020-10-22 22:38:35 +0300

    [upd] сделал первое задание шестого урока

--- lesson6/lesson6_hw1.py
+++ lesson6/lesson6_hw1.py
@@ -1,6 +1,6 @@
 """
 1. Создать класс TrafficLight (светофор) и определить у него один атрибут color (цвет) и метод running (запуск).
-Атрибут реализовать как приватный. В рамках метода реализовать переключение светофора в режимы: красный, желтый, зеленый.
+Атрибут реализовать как приватный. В рамках метода реализовать переключение светофора в режимы: красный, желтый, зеленый
 Продолжительность первого состояния (красный) составляет 7 секунд, второго (желтый) — 2 секунды,
 третьего (зеленый) — на ваше усмотрение. Переключение между режимами должно осуществляться только в указанном порядке
 (красный, желтый, зеленый). Проверить работу примера, создав экземпляр и вызвав описанный метод.
@@ -11,25 +11,39 @@
 import time
 from termcolor import colored
 
-class TrafficLight():
+
+class TrafficLight:
     # время работы каждого цвета
     time_of_work = {"red": 7, "yellow": 2, "green": 6}
-    __color = "red"
-    __current_color = __color
+    default_sleep_time = 1
+    __color = ""
+    __current_color = "current_color"
 
+    # зададим порядок следования цветов в светофоре
     correct_color_order = ["red", "yellow", "green"]
 
     def check_color_correct_order(self, old_color, current_color):
-        print(self.correct_color_order.index(current_color) - self.correct_color_order.index(old_color))
-        if self.correct_color_order.index(current_color) - self.correct_color_order.index(old_color) != 1 :
-            raise Exception("Нарушен порядок светофора")
+        if old_color != "current_color":
+            if self.correct_color_order.index(current_color) - self.correct_color_order.index(old_color) != 1:
+                raise Exception("Нарушен порядок светофора")
+        return True
 
     def running(self, color):
-        if self.check_color_correct_order(old_color=self.__current_color, current_color=color):
-            print(colored("⚫", color))
-            self.__color = color
-            self.__current_color = color
-            time.sleep(self.time_of_work[color])
+        if color in self.correct_color_order:
+            if self.check_color_correct_order(old_color=self.__current_color, current_color=color):
+                try:
+                    print(colored("⚫", color))
+                except KeyError:
+                    print(color)
+                self.__color = color
+                self.__current_color = color
+                try:
+                    time.sleep(self.time_of_work[color])
+                except KeyError:
+                    time.sleep(self.default_sleep_time)
+        else:
+            raise Exception(f"Светофор сломался, доступные цвета: {self.correct_color_order}")
+
 
 colors = ["red", "yellow", "green"]
 
