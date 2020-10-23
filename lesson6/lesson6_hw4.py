3e3558087843ad74eb7482891acca6ee80481092
sitdikov_denis <fomk@mail.ru>
2020-10-23 10:04:42 +0300

    [upd] сделал 4 задание 6 урока

--- lesson6/lesson6_hw4.py
+++ lesson6/lesson6_hw4.py
@@ -8,3 +8,54 @@
 Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print("Машина поехала")
+
+    def stop(self):
+        print("Машина остановилась")
+
+    def turn(self, direction):
+        print(f"Машина повернула {direction}")
+
+    def show_speed(self):
+        print(self.speed)
+
+
+class TownCar(Car):
+    def show_speed(self):
+        print(self.speed)
+        if self.speed > 60:
+            print("Сбавьте скорость!")
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        print(self.speed)
+        if self.speed > 40:
+            print("Сбавьте скорость!")
+
+
+class PoliceCar(Car):
+    pass
+
+
+t_car = TownCar(61, 'red', 'name1', False)
+t_car.go()
+t_car.show_speed()
+t_car.speed = 50
+t_car.show_speed()
+t_car.turn("направо")
+t_car.stop()
