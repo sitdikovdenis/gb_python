d34d3532b3015b329b7ae2d672ec461d10471cbc
sitdikov_denis <fomk@mail.ru>
2020-10-27 22:52:51 +0300

    [upd] сделал 2 задание 7 урока

--- lesson7/lesson7_hw2.py
+++ lesson7/lesson7_hw2.py
@@ -9,3 +9,32 @@
 Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания:
 реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
 """
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+    @abstractmethod
+    def get_tissue_consumption(self):
+        pass
+
+
+class Coat(Clothes):
+    def __init__(self, V):
+        self.V = V
+
+    def get_tissue_consumption(self):
+        return self.V / 6.5 + 0.5
+
+
+class Suit(Clothes):
+    def __init__(self, H):
+        self.H = H
+
+    @property
+    def get_tissue_consumption(self):
+        return 2 * self.H + 0.3
+
+
+suit = Suit(H=2)
+print(suit.get_tissue_consumption)
+print(suit.get_tissue_consumption)
